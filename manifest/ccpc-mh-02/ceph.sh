f15e5d5460cd439ebfb11e6938822dbb574f9824
Mesar Hameed <mesar.hameed@gmail.com>
2017-08-05 12:02:17 +0100

    ceph: no need to specify onwer and mode for files, as they default to root and 0600

--- manifest/ccpc-mh-02/ceph.sh
+++ manifest/ccpc-mh-02/ceph.sh
@@ -1,9 +1,9 @@
 export CDIST_ORDER_DEPENDENCY=on
 
 __package ceph
-__directory /etc/ceph --mode 0600 --owner root
-__file /etc/ceph/ceph.conf --source $__files/ceph/ceph.conf --owner root --mode 0600
-__file /etc/ceph/rbdmap --source $__files/ceph/rbdmap --owner root --mode 0600
-__encrypted_file /etc/ceph/ceph.keyring --source $__files/ceph/ceph.keyring.gpg --owner root --mode 0600 --cksum '3327662192 152'
+__directory /etc/ceph
+__file /etc/ceph/ceph.conf --source $__files/ceph/ceph.conf
+__file /etc/ceph/rbdmap --source $__files/ceph/rbdmap
+__encrypted_file /etc/ceph/ceph.keyring --source $__files/ceph/ceph.keyring.gpg --cksum '3327662192 152'
 
 unset CDIST_ORDER_DEPENDENCY
